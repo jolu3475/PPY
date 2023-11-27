68b397d9e0806765d52b070a678ce4f66ee3124a
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2023-11-27 13:15:06 +0300

    try

---
+++ test/test.py
@@ -0,0 +1,7 @@
+d = {1: "Rakoto",2:"Rabe",3:"Son"}
+#do a bocle of the start to the end of the dictionnary
+print (d[1])
+for i in range (0,5):
+    for j in range (0,i):
+        print ("*", end=" ")
+    print ()
